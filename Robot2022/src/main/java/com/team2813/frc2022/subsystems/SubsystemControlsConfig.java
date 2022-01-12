462e80d49a26dafabaa1d3e8cafe8ce782852920
abhineetpal <aip_embox@thepal.org>
2022-01-12 00:16:37 -0800

    Create SubsystemControlsConfig.java
    The SubsystemsControlsConfig class stores all the controllers, buttons, and triggers we will be using when driving and operating the robot. Right now, it only has the stuff necessary to drive the robot + the operator joystick since we haven't decided on button mappings as other subsystems are still in progress. More will be added to this class as robot development progresses.

---
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/SubsystemControlsConfig.java
@@ -0,0 +1,54 @@
+package com.team2813.frc2022.subsystems;
+
+import com.team2813.lib.controls.Axis;
+import com.team2813.lib.controls.Button;
+import com.team2813.lib.controls.Controller;
+
+/**
+ * Stores all of the controllers and their buttons and axes.
+ * Controllers are private as they should never be accessed,
+ * to make it easier to move a Button or Axis to a different
+ * controller.
+ *
+ * To add a button or axis: construct a private Button or Axis
+ * using controller.button(id). Then add a package-private
+ * getter.
+ */
+public class SubsystemControlsConfig {
+
+    // drive
+    private static Controller driveJoystick = new Controller(0);
+    private static Button pivotButton = driveJoystick.button(1);
+    private static Axis driveX = driveJoystick.axis(0);
+    private static Axis driveY = driveJoystick.axis(3);
+    private static Axis driveSteer = driveJoystick.axis(0);
+    private static Axis driveForward = driveJoystick.axis(3);
+    private static Axis driveReverse = driveJoystick.axis(2);
+
+    // operator
+    private static Controller operatorJoystick = new Controller(1);
+
+    static Button getPivotButton() {
+        return pivotButton;
+    }
+
+    static Axis getDriveX() {
+        return driveX;
+    }
+
+    static Axis getDriveY() {
+        return driveY;
+    }
+
+    static Axis getDriveSteer() {
+        return driveSteer;
+    }
+
+    static Axis getDriveForward() {
+        return driveForward;
+    }
+
+    static Axis getDriveReverse() {
+        return driveReverse;
+    }
+}
