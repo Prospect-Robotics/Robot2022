81fb034cc8b5763c2cdd1cc465be4ba70e720251
abhineetpal <aip_embox@thepal.org>
2022-01-12 16:00:28 -0800

    creating motorConfig and updating drive constants
    Updated the wheel diameter and the gear ratio for the drivetrain. I also updated the motorConfig.yaml file to have the drivetrain motors. This file will be updated as more motors are added.

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Drive.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Drive.java
@@ -5,9 +5,9 @@ import edu.wpi.first.math.util.Units;
 public class Drive {
 
     // physical constants
-    public static final double WHEEL_DIAMETER = Units.inchesToMeters(0);
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
     public static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
 
     // Autonomous
-    public static final double GEAR_RATIO = 0;
+    public static final double GEAR_RATIO = 1 / 7.64;
 }
