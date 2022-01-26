337a04186691596096f3ab6d0f535bed9747236e
abhineetpal <aip_embox@thepal.org>
2022-01-25 23:50:11 -0800

    Update Subsystems.java
    Added Shooter to list of subsystems in Subsystems.java

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Subsystems.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Subsystems.java
@@ -14,6 +14,7 @@ public class Subsystems {
 
     public static List<Subsystem> allSubsystems;
     public static Drive DRIVE;
+    public static Shooter SHOOTER;
 
     public static final Looper LOOPER = new Looper(RobotMode.DISABLED);
 
@@ -31,7 +32,8 @@ public class Subsystems {
 
     public static void initializeSubsystems() {
         DRIVE = new Drive();
-        allSubsystems = List.of(DRIVE);
+        SHOOTER = new Shooter();
+        allSubsystems = List.of(DRIVE, SHOOTER);
         LOOPER.addLoop(new SmartDashboardLoop());
     }
 
