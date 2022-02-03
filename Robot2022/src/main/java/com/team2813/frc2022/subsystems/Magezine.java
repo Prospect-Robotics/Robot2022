666a30caca34acb49f46849a65ba29c02b95e850
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2022-02-03 15:50:17 -0800

    initializing mag components

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magezine.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magezine.java
@@ -1,9 +1,40 @@
 package com.team2813.frc2022.subsystems;
 
-public class Magezine extends Subsystems {
-    //motor wrappers
+import com.team2813.frc2022.util.Units2813;
+import com.team2813.lib.config.MotorConfigs;
+import com.team2813.lib.controls.Axis;
+import com.team2813.lib.controls.Button;
+import com.team2813.lib.motors.TalonFXWrapper;
+import com.team2813.lib.motors.interfaces.ControlMode;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class Magezine extends Subsystems {
+    // physical constants
+    // motor controllers
+    private final TalonFXWrapper MOTOR; 
+
+    private final Button PIVOT_BUTTON = SubsystemControlsConfig.getPivotButton();
+    private final Axis DRIVE_X = SubsystemControlsConfig.getDriveX();
+    private final Axis DRIVE_Y = SubsystemControlsConfig.getDriveY();
+    private final Axis DRIVE_FORWARD = SubsystemControlsConfig.getDriveForward();
+    private final Axis DRIVE_REVERSE = SubsystemControlsConfig.getDriveReverse();
+
+    // private Demand demand;
+
+    public int ammo = 0;
+    public boolean triggered = false;
+    public boolean ballDetected = false;
+
+
+
+
+    //motor wrappers
     //controllers
+    
     //yaml file
 
 }
