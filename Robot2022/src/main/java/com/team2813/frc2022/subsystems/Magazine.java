0d1d9cebe1226fec0e7906fa752eb83e85805d83
Brendan Talbot <71245490+blerenderr@users.noreply.github.com>
2022-02-04 15:52:58 -0800

    added basic motor control and controllers

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magazine.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magazine.java
@@ -1,10 +1,21 @@
 package com.team2813.frc2022.subsystems;
 
+import com.team2813.lib.config.MotorConfigs;
 import com.team2813.lib.controls.Button;
+import com.team2813.lib.motors.TalonFXWrapper;
 
 public class Magazine extends Subsystem {
+    // physical constants: TODO
 
+    // motor controllers
+    private final TalonFXWrapper MAGAZINE;
+
+    // controllers
     private final Button START_STOP_BUTTON = SubsystemControlsConfig.getMagButton();
+
+    public Magazine() {
+        MAGAZINE = (TalonFXWrapper) MotorConfigs.talons.get("magazine");
+    }
 
     @Override
     public void outputTelemetry() {
