4f1687f0f4c31fbbe8d1ddccbaaf730d8763433c
Vineet Rao <60551332+vineetrao06@users.noreply.github.com>
2022-02-03 17:41:48 -0800

    controls and mag

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magazine.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Magazine.java
@@ -7,30 +7,44 @@ import com.team2813.lib.controls.Button;
 import com.team2813.lib.motors.TalonFXWrapper;
 import com.team2813.lib.motors.interfaces.ControlMode;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix.CANifier;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+// issues will get resolved when Intake branch gets merged with main
+import static com.team2813.frc2022.subsystems.Subsystems.INTAKE;
+import static com.team2813.frc2022.subsystems.Subsystems.SHOOTER;
+
 public class Magazine extends Subsystem {
     // initializing physical constants
     // initializing motor controllers
     private final TalonFXWrapper MOTOR; 
-    private final Button PIVOT_BUTTON = SubsystemControlsConfig.getPivotButton();
-    private final Axis DRIVE_X = SubsystemControlsConfig.getDriveX();
-    private final Axis DRIVE_Y = SubsystemControlsConfig.getDriveY();
-    private final Axis DRIVE_FORWARD = SubsystemControlsConfig.getDriveForward();
-    private final Axis DRIVE_REVERSE = SubsystemControlsConfig.getDriveReverse();
     private final CANifier INTAKE_COUNTER;
-    // private Demand demand;
+    private final Joystick OPERATOR_JOYSTICK = SubsystemControlsConfig.getOperatorJoystick();
+    private final Button START_STOP_BUTTON = SubsystemControlsConfig.getMagButton();
+    private final Button FORWARD_BUTTON = SubsystemControlsConfig.getMagForward();
+    private final Button REVERSE_BUTTON = SubsystemControlsConfig.getMagReverse();
+    private final Button AUTO_BUTTON = SubsystemControlsConfig.getAutoButton();
+    private Demand demand;
 
     public int ammo = 0;
     public boolean triggered = false;
     public boolean ballDetected = false;
 
 
+
     public boolean isCounterBlocked() {
         return INTAKE_COUNTER.getGeneralInput(CANifier.GeneralPin.SDA);
+    }
+
+    public void spinMagazineForward() {
+        if (SHOOTER.demand == Shooter.Demand.LOW_RANGE)
+            demand = Demand.LOW_RANGE;
+        else demand = Demand.FAR_RANGE;
+        SHOOTER.setKicker(Shooter.KickerDemand.ON);
+        INTAKE.setIntake(Intake.Demand.IN);
     }
 
 
@@ -45,10 +59,22 @@ public class Magazine extends Subsystem {
         } else triggered = false;
     }
 
+    enum Demand {
+        LOW_RANGE(1), FAR_RANGE(.5), OFF(0.0), REV(-0.3), INTAKE(0.2);
+
+        double percent;
+
+        Demand(double percent) {
+            this.percent = percent;
+        }
+    }
 
     @Override
     public void outputTelemetry() {
         // TODO Auto-generated method stub
+        SmartDashboard.putBoolean("Ball Detected", ballDetected);
+        SmartDashboard.putNumber("Ammo", ammo);
+        SmartDashboard.putNumber("Magazine Demand", demand.percent);
         
     }
 
@@ -56,6 +82,9 @@ public class Magazine extends Subsystem {
     @Override
     public void teleopControls() {
         // TODO Auto-generated method stub
+        START_STOP_BUTTON.whenPressedReleased(this::spinMagazineForward, this::stopMagazine);
+        FORWARD_BUTTON.whenPressedReleased(this::spinMagazineIntake, this::stopMagazine);
+        REVERSE_BUTTON.whenPressedReleased(this::spinMagazineReverse, this::stopMagazine);
         
     }
 
--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/SubsystemControlsConfig.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/SubsystemControlsConfig.java
@@ -4,6 +4,8 @@ import com.team2813.lib.controls.Axis;
 import com.team2813.lib.controls.Button;
 import com.team2813.lib.controls.Controller;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 /**
  * Stores all of the controllers and their buttons and axes.
  * Controllers are private as they should never be accessed,
@@ -15,18 +17,34 @@ import com.team2813.lib.controls.Controller;
  * getter.
  */
 public class SubsystemControlsConfig {
+    // driver
+	private static Controller driveJoystick = new Controller(0);
+	private static Button pivotButton = driveJoystick.button(1);
+	private static Button autoButton = driveJoystick.button(2);
+	private static Button intakeDeployButton = driveJoystick.button(4);
+	private static Button intakeSpinButton = driveJoystick.button(3);
+	private static Button magButton = driveJoystick.button(5);
+	private static Axis driveX = driveJoystick.axis(0);
+	private static Axis driveY = driveJoystick.axis(3);
+	private static Axis driveSteer = driveJoystick.axis(0);
+	private static Axis driveForward = driveJoystick.axis(3);
+	private static Axis driveReverse = driveJoystick.axis(2);
 
-    // drive
-    private static Controller driveJoystick = new Controller(0);
-    private static Button pivotButton = driveJoystick.button(1);
-    private static Axis driveX = driveJoystick.axis(0);
-    private static Axis driveY = driveJoystick.axis(3);
-    private static Axis driveSteer = driveJoystick.axis(0);
-    private static Axis driveForward = driveJoystick.axis(3);
-    private static Axis driveReverse = driveJoystick.axis(2);
-
-    // operator
-    private static Controller operatorJoystick = new Controller(1);
+	// operator
+	private static Controller operatorJoystick = new Controller(1);
+	private static Button climberPiston = operatorJoystick.button(2);
+	private static Button intakePistons = operatorJoystick.button(3);
+	private static Button intakeIn = operatorJoystick.button(5);
+	private static Button intakeOut = operatorJoystick.button(6);
+	private static Button shooterButton = operatorJoystick.button(4);
+	private static Button hoodZeroButton = operatorJoystick.button(9);
+	private static Button hoodKillButton = operatorJoystick.button(10);
+	private static Button hoodCloseButton = operatorJoystick.button(1);
+	private static Button climberButton = operatorJoystick.button(11);
+	private static Axis climberElevator = operatorJoystick.axis(1);
+	private static Button climberDisable = operatorJoystick.button(1);
+	private static Button magForward = operatorJoystick.button(7);
+	private static Button magReverse = operatorJoystick.button(8);
 
     static Button getPivotButton() {
         return pivotButton;
@@ -51,4 +69,34 @@ public class SubsystemControlsConfig {
     static Axis getDriveReverse() {
         return driveReverse;
     }
+
+    /* define these methods:
+
+    getOperatorJoystick
+    getMagButton
+    getMagForward
+    getMagReverse
+    getAutoButton
+    
+    */
+
+    static Joystick getOperatorJoystick() {
+		return operatorJoystick;
+	}
+
+    static Button getMagButton() {
+		return magButton;
+	}
+
+    static Button getMagForward() {
+		return magForward;
+	}
+
+	static Button getMagReverse() {
+		return magReverse;
+	}
+
+    static Button getAutoButton() {
+		return autoButton;
+	}
 }
