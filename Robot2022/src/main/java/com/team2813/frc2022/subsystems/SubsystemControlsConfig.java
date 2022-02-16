3d3f615d6acb1cb0c22d1c5761c6e9baed4bf778
abhineetpal <aip_embox@thepal.org>
2022-02-15 18:55:05 -0800

    adding button so there are no errors

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/SubsystemControlsConfig.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/SubsystemControlsConfig.java
@@ -28,6 +28,7 @@ public class SubsystemControlsConfig {
 
 	// operator
 	private static Controller operatorJoystick = new Controller(1);
+    private static Button intakePistonsButton = operatorJoystick.button(3);
 	private static Button intakeInButton = operatorJoystick.button(6);
 	private static Button intakeOutButton = operatorJoystick.button(5);
 	private static Button shooterButton = operatorJoystick.button(4);
@@ -54,6 +55,10 @@ public class SubsystemControlsConfig {
 
     static Axis getDriveReverse() {
         return driveReverse;
+    }
+
+    static Button getIntakePistonsButton() {
+        return intakePistonsButton;
     }
 
     static Button getShooterButton() {
