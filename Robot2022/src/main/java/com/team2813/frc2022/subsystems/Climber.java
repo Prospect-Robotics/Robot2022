7887eefd3d90607736971443557ed278280e07e7
Brendan Talbot <71245490+blerenderr@users.noreply.github.com>
2022-03-04 18:21:49 -0800

    commented out motor config, climber, and magazine

--- Robot2022/src/main/java/com/team2813/frc2022/subsystems/Climber.java
+++ Robot2022/src/main/java/com/team2813/frc2022/subsystems/Climber.java
@@ -1,16 +1,16 @@
 package com.team2813.frc2022.subsystems;
 
-import com.team2813.lib.config.MotorConfigs;
-import com.team2813.lib.motors.TalonFXWrapper;
+//import com.team2813.lib.config.MotorConfigs;
+//import com.team2813.lib.motors.TalonFXWrapper;
 
 public class Climber extends Subsystem {
 
     // Motor Controllers:
-    private final TalonFXWrapper CLIMBER;
-
-    public Climber() {
-        CLIMBER = (TalonFXWrapper) MotorConfigs.talons.get("climber");
-    }
+//    private final TalonFXWrapper CLIMBER;
+//
+//    public Climber() {
+//        CLIMBER = (TalonFXWrapper) MotorConfigs.talons.get("climber");
+//    }
 
     @Override
     public void outputTelemetry() {
